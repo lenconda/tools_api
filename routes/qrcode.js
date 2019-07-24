09ed56051d2814da39389394cba4061276430280
Lenconda <i@lenconda.top>
2019-07-24 10:52:54 +0800

    :sparkles: Finish qrcode API

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -5,11 +5,21 @@ const router = new Router({
   prefix: '/qrcode'
 })
 
-const generateBase64 = async text => await qrcode.toDataURL(text)
+const generateBase64 = async (text, options) => await qrcode.toDataURL(text, options)
 
 router.get('/:text', async (ctx, next) => {
   const text = Buffer.from(ctx.params.text, 'base64').toString()
-  ctx.body = await generateBase64(text)
+  ctx.type = 'image/png'
+  const { size, scale, margin } = ctx.query
+  const data = await generateBase64(text, 
+    {
+      width: size,
+      scale,
+      margin
+    }
+  )
+  ctx.body = Buffer.from(data.split(';base64,').pop(), 'base64')
+  console.log(ctx.body)
   await next()
 })
 
